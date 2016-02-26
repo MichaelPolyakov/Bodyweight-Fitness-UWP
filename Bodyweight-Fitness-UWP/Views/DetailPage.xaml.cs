d3e55d13d5cbe609c6372f55023fb86188fe714a
Michael Polyakov <possible.adventure@gmail.com>
2016-02-25 19:28:26 -0800

    Template10 Hamburger template

---
+++ Bodyweight-Fitness-UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Bodyweight_Fitness_UWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Bodyweight_Fitness_UWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ Bodyweight-Fitness-UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using Bodyweight_Fitness_UWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace Bodyweight_Fitness_UWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
